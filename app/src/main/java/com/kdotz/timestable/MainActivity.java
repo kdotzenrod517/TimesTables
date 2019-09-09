273dea41f414e171dc5236e0a446b1d3b1cd97da
Krista Dotzenrod <kdotzenrod517@gmail.com>
2019-09-08 21:24:29 -0500

    First commit

---
+++ app/src/main/java/com/kdotz/timestable/MainActivity.java
@@ -0,0 +1,66 @@
+package com.kdotz.timestable;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.widget.SeekBar.OnSeekBarChangeListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SeekBar sk = (SeekBar) findViewById(R.id.seekBar);
+        sk.setMax(20);
+        sk.setProgress(10);
+
+        sk.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int min = 1;
+                int timesTableNumber;
+                if(progress < min){
+                    timesTableNumber = min;
+                } else {
+                    timesTableNumber = progress;
+                }
+
+                timesTable(timesTableNumber);
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    public void timesTable(int value) {
+
+        ListView myListView = findViewById(R.id.listView);
+        List<Integer> values = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            values.add(value * i);
+        }
+
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
+        myListView.setAdapter(arrayAdapter);
+
+    }
+}
